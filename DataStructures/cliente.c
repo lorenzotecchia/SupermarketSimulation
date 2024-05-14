b49054f7c522d6cf6490c3e61788f387445cef88
Lorenzo Tecchia <rueful.chest_0f@icloud.com>
2024-05-14 15:15:07 +0200

    alcune funzioni scritte nei .h:

--- DataStructures/cliente.c
+++ DataStructures/cliente.c
@@ -1,8 +1,10 @@
 #include "cliente.h"
 
-Cliente *newCliente(){
-    Cliente *c = (Cliente *)malloc(sizeof(Cliente));
-    c->oggetti = rand()%25+1;
-    c->tempo = rand()%10+1;
-    return c;
+void aggiungiElementi(Cliente *cliente) { cliente->oggetti = rand() % 25 + 1; }
+
+Cliente *newCliente() {
+  Cliente *c = (Cliente *)malloc(sizeof(Cliente));
+  aggiungiElementi(*c);
+  c->tempo = rand() % 10 + 1;
+  return c;
 }
--- DataStructures/cliente.h
+++ DataStructures/cliente.h
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Cliente{
-    int oggetti;
-    int tempo; //in secondi
+typedef struct Cliente {
+  int oggetti;
+  int tempo; // in secondi
 } Cliente;
 
-
 Cliente *newCliente();
+
+/* Aggiunge aoggetti "cestino del cliente"
+ * Non ritorna nulla ma modifica la struct Cliente
+ * */
+void aggiungiElementi(Cliente *cliente);
+
+/* Paga gli oggetti mandando ul numero di oggetti da pagare al server
+ * Ritorna il flag di corretto invio
+ * */
+int inviaElementi(int numeroElementi, );
+
