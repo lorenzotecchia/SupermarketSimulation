d700bd943fd865240fb74b5b5392ded78e01f7f8
Lorenzo Tecchia <rueful.chest_0f@icloud.com>
2024-10-02 14:53:01 +0200

    Eliminati vari file, clean slate

--- DataStructures/cassiere.c
+++
@@ -1,8 +0,0 @@
-#include "cassiere.h"
-
-
-Cassiere *newCassiere(){
-    Cassiere *c = (Cassiere *)malloc(sizeof(Cassiere));
-    c->velocita = rand()%10+1;
-    return c;
-}
--- gas/colors.h
+++
@@ -1,4 +0,0 @@
-
-#define TEXTCOLOR_DEFAULT "\x1b[m"
-#define TEXTCOLOR_RED "\x1b[31m"
-#define TEXTCOLOR_GREEN "\x1b[32m"
