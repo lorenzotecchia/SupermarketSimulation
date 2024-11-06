6df88506e06243790fabf5ba7dbe9d8b247430cb
Simone Parente <s.parentemartone@studenti.unina.it>
2024-11-06 16:58:44 +0100

    fix dell'ultima push
    rimosse le righe aggiunte da vscode

--- client/client.c
+++ client/client.c
@@ -12,20 +12,14 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < num_clients; i++) {
     if (fork() == 0) {
       socket_fd = connect_to_server(server_name, server_port);
-<<<<<<< HEAD
 
       int time_to_shop, num_items;
       generate_client_params(&time_to_shop, &num_items);
 
-=======
-      printf("Client connesso correttamente\n");
-      int time_to_shop, num_items;
-      generate_client_params(&time_to_shop, &num_items);
       printf("Cliente generato\n");
       printf("Tempo per gli acquisti [%d]\n", time_to_shop);
       printf("Numero di oggetti [%d]\n", num_items);
       
->>>>>>> refs/remotes/origin/right
       if (request_entry_to_supermarket(socket_fd, time_to_shop, num_items) == 0) {
         shop_for_items(time_to_shop);
 
@@ -70,12 +64,8 @@ int request_entry_to_supermarket(int socket_fd, int time_to_shop, int num_items)
 }
 
 void shop_for_items(int time_to_shop) {
-<<<<<<< HEAD
-  sleep(time_to_shop);
-=======
   printf("Il cliente impiegherà [%d] secondi per fare acquisti\n", time_to_shop);
   sleep(time_to_shop);  // Simula il tempo speso a fare acquisti
->>>>>>> refs/remotes/origin/right
 }
 
 int request_queue_to_checkout(int socket_fd) {
